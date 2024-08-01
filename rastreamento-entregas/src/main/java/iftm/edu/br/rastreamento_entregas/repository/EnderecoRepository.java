5cbd77a8c48b8ca43c23bd7ad3875e40a6977d8e
Lucas <108193174+lucas-azv@users.noreply.github.com>
2024-08-01 20:38:28 -0300

    criação da cama repository

---
+++ rastreamento-entregas/src/main/java/iftm/edu/br/rastreamento_entregas/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package iftm.edu.br.rastreamento_entregas.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import iftm.edu.br.rastreamento_entregas.model.Endereco;
+
+public interface EnderecoRepository extends CrudRepository<Endereco, Long>{
+
+}
---
+++ rastreamento-entregas/src/main/java/iftm/edu/br/rastreamento_entregas/repository/PacoteRepository.java
@@ -0,0 +1,9 @@
+package iftm.edu.br.rastreamento_entregas.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import iftm.edu.br.rastreamento_entregas.model.Pacote;
+
+public interface PacoteRepository extends CrudRepository<Pacote, Long>{
+
+}
